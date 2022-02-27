a86129b77673fc431ce1eca56d4727b49f1a168e
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-02-26 23:51:17 -0300

    requsiito 03,04

--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,29 +1,89 @@
-import React, { useContext, useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useState, useEffect } from 'react';
 
 import StarContext from '../StarContext/StarContext';
 
+const defaultValues = { coluna: 'population', comparacao: 'maior que', valor: 0 };
 const Table = () => {
+  const [filterByName, setFilterByName] = useState({ name: '' });
+
+  const [filterByNumericValues, setFilterByNumericValues] = useState([]);
+  const [rulesObj, setRulesObj] = useState({ column: '', comparison: '', value: 0 });
   const { data, backupPlanets, setBackupPlanets } = useContext(StarContext);
+
   const filterPlanets = () => {
     const getNames = Object.keys(data[0]);
+
     const filterNames = getNames.filter((planetName) => planetName !== 'residents');
+
     return filterNames;
   };
 
-  const [filterByName, setFilterByName] = useState({ name: '' });
-
   const handleInput = (event) => {
     const { value } = event.target;
+
     setFilterByName({ name: value });
+
     const filterNamePlanets = data.map((planet) => planet.name);
+
     const nameFilter = filterNamePlanets.filter((planet) => planet.includes(value));
+
     const dataPlanet = data.filter((planet) => nameFilter.includes(planet.name));
+
     setBackupPlanets(dataPlanet);
+
     console.log(dataPlanet);
   };
 
+  const filterButton = () => {
+    setFilterByNumericValues([
+      ...filterByNumericValues,
+      rulesObj,
+    ]);
+    console.log(filterByNumericValues);
+  };
+
+  const handleFilterArray = (column, comparison, value, planet) => {
+    if (comparison === 'maior que') {
+      return planet[column] > Number(value);
+    }
+    if (comparison === 'menor que') {
+      return planet[column] < Number(value);
+    }
+    if (comparison === 'igual a') {
+      return planet[column] === value;
+    }
+  };
+
+  const getArray = (column, comparison, value) => backupPlanets
+    .filter((planet) => handleFilterArray(column, comparison, value, planet));
+
+  const takeInputText = (event) => {
+    const { value, name } = event.target;
+    setRulesObj({
+      ...rulesObj,
+      [name]: value });
+  };
+
+  useEffect(() => {
+    if (filterByNumericValues.length > 0) {
+      filterByNumericValues.forEach((rules) => {
+        const { column, comparison, value } = rules;
+        if (column && comparison && value) {
+          const arrayFilter = getArray(column, comparison, value);
+          setBackupPlanets(arrayFilter);
+        } else {
+          const { coluna, comparacao, valor } = defaultValues;
+          const arrayFilter = getArray(coluna, comparacao, valor);
+          setBackupPlanets(arrayFilter);
+        }
+      });
+    }
+  }, [filterByNumericValues]);
+
   return (
     <section>
+
       <div>
         {filterByName.name}
       </div>
@@ -36,6 +96,44 @@ const Table = () => {
             onChange={ handleInput }
           />
         </label>
+        <select name="column" data-testid="column-filter" onChange={ takeInputText }>
+          <option>population</option>
+          <option>orbital_period</option>
+          <option>diameter</option>
+          <option>rotation_period</option>
+          <option>surface_water</option>
+
+        </select>
+
+        <select
+          name="comparison"
+          data-testid="comparison-filter"
+          onChange={ takeInputText }
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+
+        <label htmlFor="input-number">
+          <input
+            onChange={ takeInputText }
+            name="value"
+            id="input-number"
+            type="number"
+            value={ rulesObj.value }
+            data-testid="value-filter"
+          />
+
+          <button
+            type="button"
+            data-testid="button-filter"
+            onClick={ filterButton }
+          >
+            Filtrar
+          </button>
+        </label>
+
       </form>
       <table>
         <thead>
